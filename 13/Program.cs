f7091c9a6ad3cef5a061b3a5fdc0197e39bd3dbb
dzz_cat <dzz_cat@mail.ru>
2023-04-12 15:54:13 +0400

    update var num10

--- 13/Program.cs
+++ 13/Program.cs
@@ -9,22 +9,22 @@
 // Задача требует уточнения условия, т.к. например, в данном случае максимальное вводимое число ограничено методом Convert.ToInt32 (от 0 до 4 294 967 295), 
 
 
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int numLeng = num.ToString().Length;
-    if (numLeng >= 3)
-        {
-            while (num > 999)
-            {
-                num = num / 10;
-            }
-            int num3 = num % 10;
-            Console.WriteLine($"Третья цифра введенного числа {num3} ");
-        } 
-    else
-    {
-        Console.WriteLine("Упс. Введеное число содержит менее трех цифр. Попробуйте еще раз.");
-    }
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int numLeng = num.ToString().Length;
+//     if (numLeng >= 3)
+//         {
+//             while (num > 999)
+//             {
+//                 num = num / 10;
+//             }
+//             int num3 = num % 10;
+//             Console.WriteLine($"Третья цифра введенного числа {num3} ");
+//         } 
+//     else
+//     {
+//         Console.WriteLine("Упс. Введеное число содержит менее трех цифр. Попробуйте еще раз.");
+//     }
 
 
 //  через генератор
@@ -48,3 +48,11 @@ int numLeng = num.ToString().Length;
 //         Console.WriteLine("Упс. Сгенерированное число содержит менее трех цифр. вы проиграли =)");
 //     }
 
+
+
+Console.Write("Введите число: ");
+String value = Console.ReadLine();
+int startIndex = 2;
+int length = 1;
+String substring = value.Substring(startIndex, length);
+Console.WriteLine(substring);
